84ed3a77a96be4222a0952ede80585f2c7443112
dinhquan2007 <dinhquan2007@gmail.com>
2023-05-09 20:36:43 +0700

    nap bai tap

---
+++ src/ss2_loop/bai_tap/PrintPrime.java
@@ -0,0 +1,25 @@
+package ss2_loop.bai_tap;
+
+import java.util.Scanner;
+
+public class PrintPrime {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int number= Integer.parseInt(scanner.nextLine());
+        int count=0;
+        int num=2;
+        while (count<number){
+            int count2=0;
+            for (int i = 1;i<num; i++) {
+                if (num%i==0){
+                count2++;
+                }
+            }
+            if (count2==1){
+                System.out.println(num+",");
+                count++;
+            }
+           num++;
+        }
+    }
+}
