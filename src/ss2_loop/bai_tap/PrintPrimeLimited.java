032bb1b3b8f98ba79a2b09de1b3fb2c8b4f0c48d
dinhquan2007 <dinhquan2007@gmail.com>
2023-05-10 07:03:09 +0700

    nap bai tap

--- src/ss2_loop/bai_tap/PrintPrimeLimited.java
+++ src/ss2_loop/bai_tap/PrintPrimeLimited.java
@@ -1,17 +1,19 @@
 package ss2_loop.bai_tap;
+
 public class PrintPrimeLimited {
     public static void main(String[] args) {
         for (int i = 2; i < 100; i++) {
-            if (isPrime(i)){
-                System.out.println(i+",");
+            if (isPrime(i)) {
+                System.out.println(i + ",");
             }
         }
     }
+
     //kiểm tra theo hàm kiểu boolean
-    public static  boolean isPrime(int n) {
+    public static boolean isPrime(int n) {
         for (int i = 2; i < n; i++) {
-            if (n % i==0) {
-              return false;
+            if (n % i == 0) {
+                return false;
             }
         }
         return true;
