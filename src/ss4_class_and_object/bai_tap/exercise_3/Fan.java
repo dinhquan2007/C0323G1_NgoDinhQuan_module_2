b6def8a7d294e5a08e80c655c4b4545ffb43092c
dinhquan2007 <dinhquan2007@gmail.com>
2023-05-12 10:38:47 +0700

    nap bai tap

--- src/ss4_class_and_object/bai_tap/exercise_3/Fan.java
+++ src/ss4_class_and_object/bai_tap/exercise_3/Fan.java
@@ -8,28 +8,81 @@ public class Fan {
     private boolean on;
     private double radius;
     private String color;
-    public Fan(int speed,int radius,String color,boolean status){
-        this.speed=speed;
-        this.radius=radius;
-        this.color=color;
-        this.on=status;
+
+    public Fan(int speed, int radius, String color, boolean status) {
+        this.speed = speed;
+        this.radius = radius;
+        this.color = color;
+        this.on = status;
     }
-    public Fan(int speed){
+
+    public Fan(int speed) {
         getSpeed(speed);
-        this.on= false;
-        this.color="blue";
-        this.radius=5;
+        this.on = false;
+        this.color = "blue";
+        this.radius = 5;
     }
+
     public String getSpeed(int n) {
         switch (n) {
             case 1:
-                return SLOW;
+                return getSLOW();
             case 2:
-                return MEDIUM;
+                return getMEDIUM();
             case 3:
-                return FAST;
+                return getFAST();
         }
-        return "off";
+        return "0";
+    }
+
+    public String getSLOW() {
+        return SLOW;
+    }
+
+    public String getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public String getFAST() {
+        return FAST;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Fan{" +
+                ", speed=" + getSpeed(speed) +
+                ", on=" + on +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
 
--- src/ss4_class_and_object/bai_tap/exercise_3/Main.java
+++ src/ss4_class_and_object/bai_tap/exercise_3/Main.java
@@ -1,6 +1,10 @@
 package ss4_class_and_object.bai_tap.exercise_3;
 
 public class Main {
-    Fan fan1= new Fan(3,10,"yellow",true);
-    Fan fan2= new Fan(2);
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3, 10, "yellow", true);
+        Fan fan2 = new Fan(2);
+        System.out.println(fan1);
+        System.out.println(fan2);
+    }
 }
